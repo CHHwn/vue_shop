f332ff8c4c45f5f3db7de6740e1f181c04700d26
CHHwn <8907375+chwwen@user.noreply.gitee.com>
2022-08-21 22:26:43 +0800

    完成了订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,6 +3,10 @@ import Element, { Message } from 'element-ui'
 import { MessageBox } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+
+
 Vue.use(Element)
+
+
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
